fe3504832aa98d6b4183e195dc27861d0e67bf19
Kings-Canon <bushnellaugoh@gmail.com>
2023-07-02 09:44:29 +0300

    Update 1

--- Classes.py
+++ Classes.py
@@ -1,5 +1,6 @@
 #This project is based on a law firm. I am going to create a Class to actualize this
 class Advocate:
+    raise_bonus  = 1.45
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -7,29 +8,29 @@ class Advocate:
         self.pay = pay
         self.email = first + "." + last + "@aocllp.com"
     
+    #Method that returns the fullname of the advocate
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    #Method that calculates the pay after the raise bonus
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_bonus)
+    
+
     
 adv1 = Advocate("Elton", "Alwanga", 80000)
 adv2 = Advocate("David", "Ogara", 75000)
 adv3 = Advocate("Kings", "Canon", 70000)
 
 
-print(adv1.first)
-print(adv1.last)
-print(adv1.email.lower())
+print(adv1.pay)
+adv1.apply_raise()
 print(adv1.pay)
 
-print(adv2.first)
-print(adv2.last)
-print(adv2.email.lower())
+print(adv2.pay)
+adv2.apply_raise()
 print(adv2.pay)
 
-print(adv3.first)
-print(adv3.last)
-print(adv3.email.lower())
 print(adv3.pay)
-
-print(adv1.fullname())
-print(adv2.fullname())
-print(adv3.fullname())
+adv3.apply_raise()
+print(adv3.pay)
