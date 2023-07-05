719f4dc901f2b2263deb6bed9f3b2d3ccde8f4ef
Kings-Canon <bushnellaugoh@gmail.com>
2023-07-05 21:02:35 +0300

    Update 2

--- Classes.py
+++ Classes.py
@@ -16,21 +16,30 @@ class Advocate:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_bonus)
     
-
+    #Using a classmethod to instantiate the strings to objects of the Advocate class
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, int(pay))
+    
     
 adv1 = Advocate("Elton", "Alwanga", 80000)
 adv2 = Advocate("David", "Ogara", 75000)
 adv3 = Advocate("Kings", "Canon", 70000)
 
+#Let's say we have strings representing advocate details and we want to instantiate them as objects of the class
+adv4_str = "Sandra-Ataka-65000"
+adv5_str = "David-Masinde-50000"
 
-print(adv1.pay)
-adv1.apply_raise()
-print(adv1.pay)
+adv4 = Advocate.from_string(adv4_str)
+adv5 = Advocate.from_string(adv5_str)
 
-print(adv2.pay)
-adv2.apply_raise()
-print(adv2.pay)
+print(adv4.fullname())
+print(adv4.pay)
+adv4.apply_raise()
+print(adv4.pay)
 
-print(adv3.pay)
-adv3.apply_raise()
-print(adv3.pay)
+print(adv5.fullname())
+print(adv5.pay)
+adv5.apply_raise()
+print(adv5.pay)
